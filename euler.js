a5eac9fdcb3a97028461c1107863a795b290d7c0
Joncygeorge <121477511+Joncygeorge@users.noreply.github.com>
2024-09-30 10:11:33 +0100

    functioneuler

--- euler.js
+++ euler.js
@@ -1,4 +1,5 @@
 // Find the sum of all the multiples of 3 or 5 below 1000
+function sumofMultiples(a,b,n){
 let sum =0
 for(let i = 0; i<1000; i++){
     if(i%3===0||i%5===0){
@@ -6,3 +7,12 @@ for(let i = 0; i<1000; i++){
     }
 }
 alert(sum);
+}
+
+function cal(){
+    const a = parseInt(document.getElementById('a').value);
+    const b= parseInt(document.getElementById('b').value);
+    const c = parseInt(document.getElementById('c').value);
+    const result= sumofMultiples(a,b,n)
+    alert('the sum of multiples')
+}
---
+++ function.js
@@ -0,0 +1,11 @@
+// create a  function to find the sum of a and b, below n
+// hence a, b and n are function parameters
+function sum(a, b,n ){
+    let sum = 0;
+    for(let i=0;i<n;i++){
+       // if(i%a===0 || i%b===0){//
+            sum+=i
+       // }//
+    }
+    return sum;
+}
