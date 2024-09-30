7c16349f190faa3c549da088ddd706607bc069f8
Joncygeorge <121477511+Joncygeorge@users.noreply.github.com>
2024-09-30 11:10:13 +0100

    commiting function changes

--- euler.js
+++ euler.js
@@ -1,18 +1,22 @@
 // Find the sum of all the multiples of 3 or 5 below 1000
+alert("Loaded Euler JS");
+a=3;
+b=5;
+n=1000;
 function sumofMultiples(a,b,n){
 let sum =0
-for(let i = 0; i<1000; i++){
-    if(i%3===0||i%5===0){
+for(let i = 1; i<n; i++){
+    if(i%a==0||i%b==0){
         sum+=i
     }
 }
-alert(sum);
+return(sum);
 }
 
 function cal(){
     const a = parseInt(document.getElementById('a').value);
     const b= parseInt(document.getElementById('b').value);
-    const c = parseInt(document.getElementById('c').value);
+    const c = parseInt(document.getElementById('n').value);
     const result= sumofMultiples(a,b,n)
-    alert('the sum of multiples')
+    alert('The sum of multiples of numbers ${a} or ${b} below ${n} is: ${result}')
 }
