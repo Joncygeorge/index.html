47a0a8cdbcfb2855253f4293d27995b5dee03587
Joncygeorge <121477511+Joncygeorge@users.noreply.github.com>
2024-10-06 19:49:40 +0100

    commiting 2nd array problem

--- array.js
+++ array.js
@@ -1,14 +1,29 @@
-let l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let a = 2;
-let b = 3;
-function sumOfMultiples(a, b, l) {
-    reduce((sum, num) => {
-        if (num % a === 0 || num % b === 0) {
+// let l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let a = 2;
+// let b = 3;
+// function sumOfMultiples(a, b, l) {
+//     reduce((sum, num) => {
+//         if (num % a === 0 || num % b === 0) {
+//             return sum + num;
+//         }
+//         return sum;
+//     }, 0);
+// }
+
+// let result = sumOfMultiples(a, b, l);
+// alert("Final sum of multiples of " + a + " or " + b + " is: " + result);  // Output will be the sum of multiples of 2 or 3 in the list
+
+
+let a=[2,3]
+let l=[1,2,3,4,5,6,7,8,9,10]
+
+function sumOfMultiples(a,l){
+    filter((sum,num)=>{
+        if(num%a===0){
             return sum + num;
         }
         return sum;
-    }, 0);
+    }, 0)
 }
-
-let result = sumOfMultiples(a, b, l);
-alert("Final sum of multiples of " + a + " or " + b + " is: " + result);  // Output will be the sum of multiples of 2 or 3 in the list
+let result= sumOfMultiples(a,l);
+alert("Final sum of multiples of " + a + " is: " + result)
