7e30b64e2bb5cd62ea6f90889dc2f20716fc5536
Joncygeorge <121477511+Joncygeorge@users.noreply.github.com>
2024-10-06 19:10:33 +0100

    commiting changes

--- array.js
+++ array.js
@@ -1,3 +1,6 @@
+let l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let a = 2;
+let b = 3;
 function sumOfMultiples(a, b, l) {
     return l.reduce((sum, num) => {
         if (num % a === 0 || num % b === 0) {
@@ -7,9 +10,5 @@ function sumOfMultiples(a, b, l) {
     }, 0);
 }
 
-// Example usage:
-let l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let a = 2;
-let b = 3;
 let result = sumOfMultiples(a, b, l);
 console.log(result);  // Output will be the sum of multiples of 2 or 3 in the list
