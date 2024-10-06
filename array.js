bc917f78b850fffb1b84d703c3c6f1c2d9017b23
Joncygeorge <121477511+Joncygeorge@users.noreply.github.com>
2024-10-06 19:56:39 +0100

    changes in array

--- array.js
+++ array.js
@@ -2,7 +2,7 @@ let l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let a = 2;
 let b = 3;
 function sumOfMultiples(a, b, l) {
-    reduce((sum, num) => {
+    return l.reduce((sum, num) => {
         if (num % a === 0 || num % b === 0) {
             return sum + num;
         }
